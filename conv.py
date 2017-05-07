f47b551a464d655370f0a97765463f53708b75a4
Gustavo Santos <Gustavo Santos>
2017-05-07 01:26:27 -0300

    2nd version: read tabs

--- conv.py
+++ conv.py
@@ -1,3 +1,5 @@
+import math
+
 class Conv:
     def __init__(self):
         self.guitar_dic = []
@@ -34,8 +36,8 @@ class Conv:
         self.ukulele_dic.append(list(reversed(['G', 'C', 'E', 'A'])))
 
     def find_note_guitar(self, fret, string):
+        # print("guitar: {}:{}".format(fret, string + 1))
         note = self.guitar_dic[fret][string]
-        print("Note: {}".format(note))
         return note
 
     def find_eq_uku(self, note):
@@ -45,6 +47,30 @@ class Conv:
                 if self.ukulele_dic[i][j] == note:
                     notes.append("{}:{}".format(i, j + 1))
         return notes
+
+    def guitar_note_is_valid(self, instrument, note):
+        fret, string = note.split(':')
+        # is valid
+        if 12 >= fret >= 0 and 6 >= string >= 1:
+            return True
+        # isn't
+        else:
+            return False
+
+    def select_closest_eq_note_gtu(self, note_guitar, eq_ukulele):
+        if note_guitar != None and len(eq_ukulele) > 0:
+            closest = None
+            dif_closest = math.inf
+            fret_g, string_g = note_guitar.split(':')
+            for note in eq_ukulele:
+                fret_eq, string_eq = note.split(':')
+                dif_now = abs(int(fret_g) - int(fret_eq))
+                if dif_now < dif_closest:
+                    closest = note
+                    dif_closest = dif_now
+            return closest
+        else:
+            return None
 
     def print_guitar(self):
         print("\n     Standard tunning")
@@ -58,12 +84,6 @@ class Conv:
     def print_ukulele(self):
         print("\n     Soprano standard tunning")
         print("-----------------------------------")
-        """
-        for i in range(4):
-            print("{0:5d}   ".format(i), end='')
-        print()
-        print("-----------------------------------")
-        """
         for i in range(13):
             print("{0:3} | ".format(i), end='')
             for j in range(4):
@@ -75,7 +95,8 @@ class Conv:
         Pass fret:string position or note to convert to ukulele fret:position
 
         How to use?
-        guitar_to_ukulele("1:2") or guitar_to_ukulele("A#")
+        guitar_to_ukulele("1:2") or guitar_to_ukulele("A#") or 
+        guitar_to_ukulele("Eb") or guitar_to_ukulele("F#/Gb")
 
         and will return: ["1:4"] or ["1:4", "3:1", "6:3", "10:2"]
         """
@@ -84,7 +105,8 @@ class Conv:
             fret, string = ng.split(':')
             note = self.find_note_guitar(int(fret), int(string) - 1)
             fs = self.find_eq_uku(note)
-            return fs
+            right = self.select_closest_eq_note_gtu(ng, fs)
+            return right
         elif len(ng) == 2:
             return None
         elif len(ng) == 5 and ng.find('/') >= 0:
@@ -92,6 +114,235 @@ class Conv:
         else:
             print("[ERROR] Format not especified.")
             return None
+
+    def print_tab_guitar(self, tab_guitar):
+        number_patterns = int(len(tab_guitar)/6)
+        for p in range(number_patterns):
+            print()
+            print(tab_guitar[6*p], end='')
+            print(tab_guitar[6*p + 1], end='')
+            print(tab_guitar[6*p + 2], end='')
+            print(tab_guitar[6*p + 3], end='')
+            print(tab_guitar[6*p + 4], end='')
+            print(tab_guitar[6*p + 5], end='')
+            print()
+
+    def print_tab_ukulele(self, tab_ukulele):
+        number_patterns = int(len(tab_ukulele)/4)
+        for p in range(number_patterns):
+            print()
+            print(tab_ukulele[4*p])
+            print(tab_ukulele[4*p + 1])
+            print(tab_ukulele[4*p + 2])
+            print(tab_ukulele[4*p + 3])
+            print()
+
+    def process_tab_guitar(self, tab_guitar):
+        number_patterns = int(len(tab_guitar)/6)
+
+        # inicialization of ukulele tablature
+        tab_ukulele = []
+
+        uk = []
+        for pattern in range(number_patterns):
+            if tab_guitar[6*pattern][0] == '-':
+                start_point = 0
+            elif tab_guitar[6*pattern][0] == '|':
+                start_point = 1
+            elif tab_guitar[6*pattern][0] == 'e':
+                start_point = 2
+
+            st1 = tab_guitar[6*pattern]
+            st2 = tab_guitar[6*pattern + 1]
+            st3 = tab_guitar[6*pattern + 2]
+            st4 = tab_guitar[6*pattern + 3]
+            st5 = tab_guitar[6*pattern + 4]
+            st6 = tab_guitar[6*pattern + 5]
+
+            uk.append(list("A|{}|".format(''.join(['-' for l in range(len(st1) - 4)]))))
+            uk.append(list("E|{}|".format(''.join(['-' for l in range(len(st1) - 4)]))))
+            uk.append(list("C|{}|".format(''.join(['-' for l in range(len(st1) - 4)]))))
+            uk.append(list("G|{}|".format(''.join(['-' for l in range(len(st1) - 4)]))))
+
+            stop = len(tab_guitar[6*pattern]) - 2
+            i = start_point
+
+            flag_dig_s1 = False
+            flag_dig_s2 = False
+            flag_dig_s3 = False
+            flag_dig_s4 = False
+            flag_dig_s5 = False
+            flag_dig_s6 = False
+            while i < stop:
+               # print("i = {}".format(i))
+                # e
+                if st1[i].isdecimal():
+                    if flag_dig_s1:
+                        flag_dig_s1 = False
+                    else:
+                        if st1[i+1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st1[i], st1[i+1]]), 1))
+                            flag_dig_s1 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st1[i], 1))
+                        fret, string = eq.split(':')
+                        print("ukulele: {}:{}".format(fret, string))
+                        if int(fret) >= 10:
+                            if st1[i+1].isdecimal():
+                                uk[int(string) - 1][i-1] = fret[0]
+                                uk[int(string) - 1][i] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret[0]
+                                uk[int(string) - 1][i+1] = fret[1]
+                        else:
+                            uk[int(string) - 1][i] = fret
+
+                # B
+                if st2[i].isdecimal():
+                    if flag_dig_s2:
+                        flag_dig_s2 = False
+                    else:
+                        if st2[i + 1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st2[i], st2[i+1]]), 2))
+                            flag_dig_s2 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st2[i], 2))
+                        fret, string = eq.split(':')
+                        #print("ukulele: {}:{}".format(fret, string))
+                        if int(fret) >= 10:
+                            if st2[i+1].isdecimal():
+                                uk[int(string) - 1][i-1] = fret[0]
+                                uk[int(string) - 1][i] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret[0]
+                                uk[int(string) - 1][i+1] = fret[1]
+                        else:
+                            uk[int(string) - 1][i] = fret
+
+                # G
+                if st3[i].isdecimal():
+                    if flag_dig_s3:
+                        flag_dig_s3 = False
+                    else:
+                        if st3[i + 1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st3[i], st3[i+1]]), 3))
+                            flag_dig_s3 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st3[i], 3))
+                        fret, string = eq.split(':')
+                        ##print("ukulele: {}:{}".format(fret, string))
+                        if int(fret) > 10:
+                            if st3[i+1].isdecimal():
+                                uk[int(string) - 1][i-1] = fret[0]
+                                uk[int(string) - 1][i] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret[0]
+                                uk[int(string) - 1][i+1] = fret[1]
+                        else:
+                            uk[int(string) - 1][i] = fret
+
+                # D
+                if st4[i].isdecimal():
+                    if flag_dig_s4:
+                        flag_dig_s4 = False
+                    else:
+                        if st4[i + 1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st4[i], st4[i+1]]), 4))
+                            flag_dig_s4 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st4[i], 4))
+                            flag_dig_s4 = False
+                        if eq != None:
+                            fret, string = eq.split(':')
+                            #print("ukulele: {}:{}".format(fret, string))
+                            if int(fret) > 10:
+                                if st4[i+1].isdecimal():
+                                    uk[int(string) - 1][i-1] = fret[0]
+                                    uk[int(string) - 1][i] = fret[1]
+                                else:
+                                    uk[int(string) - 1][i] = fret[0]
+                                    uk[int(string) - 1][i+1] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret
+
+                # A
+                if st5[i].isdecimal():
+                    if flag_dig_s5:
+                        flag_dig_s5 = False
+                    else:
+                        if st5[i + 1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st5[i], st5[i+1]]), 5))
+                            flag_dig_s5 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st5[i], 5))
+                            flag_dig_s5 = False
+                        if eq != None:
+                            fret, string = eq.split(':')
+                            # print("ukulele: {}:{}".format(fret, string))
+                            if int(fret) > 10:
+                                if st5[i+1].isdecimal():
+                                    uk[int(string) - 1][i-1] = fret[0]
+                                    uk[int(string) - 1][i] = fret[1]
+                                else:
+                                    uk[int(string) - 1][i] = fret[0]
+                                    uk[int(string) - 1][i+1] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret
+
+                # E
+                if st6[i].isdecimal():
+                    if flag_dig_s6:
+                        flag_dig_s6 = False
+                    else:
+                        if st6[i + 1].isdecimal():
+                            eq = self.guitar_to_ukulele("{}:{}".format(''.join([st6[i], st6[i+1]]), 6))
+                            flag_dig_s6 = True
+                        else:
+                            eq = self.guitar_to_ukulele("{}:{}".format(st6[i], 6))
+                        if eq != None:
+                            fret, string = eq.split(':')
+                            #print("ukulele: {}:{}".format(fret, string))
+                            if int(fret) > 10:
+                                if st6[i+1].isdecimal():
+                                    uk[int(string) - 1][i-1] = fret[0]
+                                    uk[int(string) - 1][i] = fret[1]
+                                else:
+                                    uk[int(string) - 1][i] = fret[0]
+                                    uk[int(string) - 1][i+1] = fret[1]
+                            else:
+                                uk[int(string) - 1][i] = fret
+                i = i + 1
+
+            tab_ukulele.append(''.join(uk[0]))
+            tab_ukulele.append(''.join(uk[1]))
+            tab_ukulele.append(''.join(uk[2]))
+            tab_ukulele.append(''.join(uk[3]))
+            uk = []
+
+        return tab_ukulele
+
+    def tabguitar_to_tabukulele(self, tab_guitar):
+        """
+        Usage:
+            tab_guitar is a file that contains ONLY the tab for guitar
+
+            tabguitar_to_tabukulele('file_with_tab.txt')
+
+            and return a text with equivalent tab on ukulele
+        """
+        print("Processing \'{}\'...".format(tab_guitar))
+        file = open(tab_guitar, 'r')
+        
+        if file.readable():
+            tab = file.readlines()
+            tab = [line for line in tab if len(line) > 1]
+            self.print_tab_guitar(tab)
+            if len(tab)%6 == 0:
+                tab_ukulele = self.process_tab_guitar(tab)
+                return tab_ukulele
+            else:
+                print("[ERROR] Wrong tab pattern")
+                return None
 
     def ukulele_to_guitar(self, nu):
         pass
@@ -101,6 +352,9 @@ c.print_guitar()
 c.print_ukulele()
 #print(c.guitar_to_ukulele('Bb'))
 #print(c.guitar_to_ukulele('F#'))
-print(c.guitar_to_ukulele('2:5'))
-print(c.guitar_to_ukulele('2:4'))
+print(c.guitar_to_ukulele('8:4'))
+print(c.guitar_to_ukulele('6:3'))
 print(c.guitar_to_ukulele('0:3'))
+c.print_tab_ukulele(c.tabguitar_to_tabukulele('belivier_teste.txt'))
+c.print_tab_ukulele(c.tabguitar_to_tabukulele('where_is_my_mind.txt'))
+c.print_tab_ukulele(c.tabguitar_to_tabukulele('ghost_towns.txt'))
